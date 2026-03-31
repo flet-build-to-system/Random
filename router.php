37426240b7f048eebc5620f0dc58360f8b975660
flet-build-to-system <abdelhaqelhafidy@gmail.com>
2026-03-31 07:30:58 -0700

    Update router.php

--- router.php
+++ router.php
@@ -1,13 +1,10 @@
 <?php
 
-// إذا الملف موجود (css, js, images...) خليه يخدم عادي
-if (php_sapi_name() == 'cli-server') {
-    $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $file = __DIR__ . $url;
-    if (is_file($file)) {
-        return false;
-    }
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$file = __DIR__ . $path;
+
+if ($path !== "/" && file_exists($file)) {
+    return false;
 }
 
-// الصفحة الرئيسية
 echo "Bot is running 🚀";
